0211d6c5be733a7463b213a71f81dc4e2d6fd62d
Macoar20 <114072086+Macoar20@users.noreply.github.com>
2022-10-24 22:02:44 +0200

    Update Ejercicio05.java

--- EjercicioT2-ManuelCosano/src/ejercicios/Ejercicio05.java
+++ EjercicioT2-ManuelCosano/src/ejercicios/Ejercicio05.java
@@ -3,22 +3,30 @@ package ejercicios;
 import java.util.Scanner;
 
 public class Ejercicio05 {
+	/* He realizado 2 pruebas en este ejercicio
+	* He introducido un número positivo, el 9 el cual como resultado me ha dado el 9
+	* He introducido un número negativo, el -6 el cual como resultado me ha dado el 6
+	*/
 
 	public static void main(String[] args) {
+		// Introducimos las variables
+		int numero; // Esta variable hará referencia al número introducido por el usuario
+		int vabsoluto; // Esta variable hará referencia al número en valor absoluto
 		
-		int numero;
-		int vabsoluto;
-		
+		// Creamos el escáner que nos permitirá leer las variables
 		Scanner ma = new Scanner(System.in);
 		
+		// Le pedimos al usuario que introduzca un número
 		System.out.println("Introduzca un número por favor");
 		
-		numero = ma.nextInt();
+		numero = ma.nextInt(); // Le damos un valor a la variable número
 		
-		vabsoluto = numero > 0 ? numero : -numero;
+		vabsoluto = numero > 0 ? numero : -numero; // Mediante un ternario obtendremos el valor del número absoluto
 		
+		// Imprimimos el número en valor absoluto con el mensaje
 		System.out.println("Tú número en valor absoluto es "+ vabsoluto);
 		
+		//Cerramos el escáner
 		ma.close();
 	}
 
